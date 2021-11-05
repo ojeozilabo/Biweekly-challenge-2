a3f7f5e50dec355166f86caa98905b5b4b9a76f9
Chukwuemeka Kanu <mekuseo@gmail.com>
2021-11-05 08:01:18 +0100

    converted from .ipynb to .py

---
+++ Chukwuemeka Kanu/Bi-weekly challenge2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+def age_strate(input):
+    name_input = input('input your name: ')
+    age_input = int(input('input your age: '))
+    print('Hello', name_input,', you are', age_input, 'years old')
+
+    dob = 2021 - age_input
+    print('Your year of Birth is', dob)
+
+    if age_input<1:
+        print('As you are', age_input, 'years old, you are an Infant')
+    elif age_input<=11:
+        print('As you are', age_input, 'years old, you a an Child')
+    elif age_input<=17:
+        print('As you are', age_input, 'years old, you are a Teen')
+    else:
+        print('As you are', age_input, 'years old, you are an Adult')
+
+
+    decades = range(-10,51,10)
+    decade_age = 0
+    for i in decades:
+        decade_calculation = 2021 + i
+        decade_age = age_input + i
+        print('In', decade_calculation, 'you were', decade_age, 'years old')
+
+        
+
+age_strate(input)
+
+
+# In[ ]:
+
+
+
+
