c7455dfaf3a590be47bd0c69de2651cac37b3a9f
Chukwuemeka Kanu <37825115+Mekuseo@users.noreply.github.com>
2021-11-05 11:33:23 +0100

    Update Bi-weekly challenge2.py

--- Chukwuemeka Kanu/Bi-weekly challenge2.py
+++ Chukwuemeka Kanu/Bi-weekly challenge2.py
@@ -4,35 +4,31 @@
 # In[1]:
 
 
+name_input = input('input your name: ')
+age_input = int(input('input your age: '))
+print('Hello', name_input,', you are', age_input, 'years old')
 
-def age_strate(input):
-    name_input = input('input your name: ')
-    age_input = int(input('input your age: '))
-    print('Hello', name_input,', you are', age_input, 'years old')
+dob = 2021 - age_input
+print('Your year of Birth is', dob)
 
-    dob = 2021 - age_input
-    print('Your year of Birth is', dob)
-
-    if age_input<1:
-        print('As you are', age_input, 'years old, you are an Infant')
-    elif age_input<=11:
-        print('As you are', age_input, 'years old, you a an Child')
-    elif age_input<=17:
-        print('As you are', age_input, 'years old, you are a Teen')
-    else:
-        print('As you are', age_input, 'years old, you are an Adult')
+if age_input<1:
+    print('As you are', age_input, 'years old, you are an Infant')
+elif age_input<=11:
+    print('As you are', age_input, 'years old, you a an Child')
+elif age_input<=17:
+    print('As you are', age_input, 'years old, you are a Teen')
+else:
+    print('As you are', age_input, 'years old, you are an Adult')
 
 
-    decades = range(-10,51,10)
-    decade_age = 0
-    for i in decades:
-        decade_calculation = 2021 + i
-        decade_age = age_input + i
-        print('In', decade_calculation, 'you were', decade_age, 'years old')
+decades = range(-10,51,10)
+decade_age = 0
+for i in decades:
+    decade_calculation = 2021 + i
+    decade_age = age_input + i
+    print('In', decade_calculation, 'you were', decade_age, 'years old')
 
         
-
-age_strate(input)
 
 
 # In[ ]:
